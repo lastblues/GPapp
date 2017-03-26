c10ef3ca60bd1e753e102c49a1f3ccfde7437a1c
dmac182 <dmacphail182@gmail.com>
2017-03-25 18:25:58 -0700

    Attempt to add in app YouTube video viewing
    Causes unkown crash

---
+++ app/src/main/java/com/example/andyshin/gpapp/Config.java
@@ -0,0 +1,14 @@
+package com.example.andyshin.gpapp;
+
+
+/**
+ * Created by darren on 2017-03-25.
+ */
+
+public class Config {
+
+    private Config(){
+    }
+
+    public static final String YOUTUBE_API_KEY = "AIzaSyAJt-EK56Nkm7BvOzY8jaENlR3FW05R0x4";
+}
--- app/src/main/java/com/example/andyshin/gpapp/EmbedActivity.java
+++ app/src/main/java/com/example/andyshin/gpapp/EmbedActivity.java
@@ -1,5 +1,6 @@
 package com.example.andyshin.gpapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.webkit.WebView;
@@ -7,53 +8,61 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.webkit.WebSettings;
 import android.webkit.WebViewClient;
+import android.widget.Toast;
+
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayer.Provider;
+import com.google.android.youtube.player.YouTubePlayerView;
 
 /**
  * Created by Andy Shin on 3/15/2017.
  */
 
-public class EmbedActivity extends AppCompatActivity{
+public class EmbedActivity extends YouTubeBaseActivity implements YouTubePlayer.OnInitializedListener{
+
+    private static final int RECOVERY_REQUEST = 1;
+    private YouTubePlayerView youTubeView;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_videoplayback);
+        setContentView(R.layout.activity_main);
 
-        //declare the embede video
-        String frameVideo = "<html><body>Video From YouTube<br><iframe width=\"420\" height=\"315\" src=\"https://www.youtube.com/embed/47yJ2XCRLZs\" frameborder=\"0\" allowfullscreen></iframe></body></html>";
-
-        WebView showVideo = (WebView) findViewById(R.id.mWebView);
-
-        showVideo.setWebViewClient(new WebViewClient(){
-
-            @Override
-            public boolean shouldOverrideUrlLoading(WebView view, String url){
-                return false;
-            }
-        });
-
-        WebSettings webSettings = showVideo.getSettings();
-        webSettings.setJavaScriptEnabled(true);
-        showVideo.loadData(frameVideo, "text/html", "utf-8");
+        youTubeView = (YouTubePlayerView) findViewById(R.id.youtube_view);
+        youTubeView.initialize(Config.YOUTUBE_API_KEY, this);
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu){
+    public void onInitializationSuccess(Provider provider, YouTubePlayer player, boolean wasRestored) {
+        player.cueVideo("fhWaJi1Hsfo");
 
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+        if (!wasRestored) {
+            player.cueVideo("fhWaJi1Hsfo"); // Plays https://www.youtube.com/watch?v=fhWaJi1Hsfo
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item){
-        int id = item.getItemId();
-
-        if(id == R.id.action_settings){
-            return true;
         }
-
-        return super.onOptionsItemSelected(item);
     }
 
+    @Override
+    public void onInitializationFailure(Provider provider, YouTubeInitializationResult errorReason) {
+        if (errorReason.isUserRecoverableError()) {
+            errorReason.getErrorDialog(this, RECOVERY_REQUEST).show();
+        } else {
+            String error = String.format(getString(R.string.player_error), errorReason.toString());
+            Toast.makeText(this, error, Toast.LENGTH_LONG).show();
+        }
+    }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == RECOVERY_REQUEST) {
+            // Retry initialization if user performed a recovery action
+            getYouTubePlayerProvider().initialize(Config.YOUTUBE_API_KEY, this);
+        }
+    }
+
+    protected Provider getYouTubePlayerProvider() {
+        return youTubeView;
+    }
 }
